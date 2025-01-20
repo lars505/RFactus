7cfcf52db73e3fb99236b1f7228d9c56aeb5319c
Angel Lars <lars50593@gmail.com>
2025-01-19 23:09:13 -0600

    ✨ feat(models): create Product model and configure image settings in settings.py

--- factus/models.py
+++ factus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=50, )
+    image = models.ImageField(upload_to='products', null=True, blank=True)
+    price = models.IntegerField( )
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} - {self.price} - {self.stock}'
+    
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
