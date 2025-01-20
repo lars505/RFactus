c1ecf251c830b2568550ddf7bb3563eae8bde875
Angel Lars <lars50593@gmail.com>
2025-01-20 00:47:04 -0600

    🗂️ feat(models): add Category model and link it to Product as a foreign key

--- factus/models.py
+++ factus/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
 
 class Products(models.Model):
     name = models.CharField(max_length=50, )
     image = models.ImageField(upload_to='products', null=True, blank=True)
-    price = models.IntegerField( )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
 
     def __str__(self):
         return f'{self.name} - {self.price} - {self.stock}'
@@ -14,3 +25,5 @@ class Products(models.Model):
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
+
+
