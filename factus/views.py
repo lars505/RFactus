8cb0729d20f1b28243d1a6819ac452f2e6859d11
Angel Lars <lars50593@gmail.com>
2025-01-19 23:51:01 -0600

    ✨ feat: add static folder, create base.html and index.html templates, and generate index view

--- factus/views.py
+++ factus/views.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
+from .models import Products
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'factus/index.html')
