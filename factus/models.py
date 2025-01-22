20273c3ddab7db3d7ad336238a8a14482266bf0f
Angel Lars <lars50593@gmail.com>
2025-01-21 18:05:44 -0600

    🛒 feat(cart): complete user cart functionality, update product list and description styles, and add user field to Product model

--- factus/models.py
+++ factus/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=25)
@@ -15,7 +15,7 @@ class Category(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length=50, )
     image = models.ImageField(upload_to='products', null=True, blank=True)
-    descriotion = models.TextField( blank=True, null=True)
+    description = models.TextField( blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
@@ -45,9 +45,10 @@ class Products(models.Model):
 
 class Cart(models.Model):
     products = models.ManyToManyField(Products, through='CartProducts')
-    descuento = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    IVA = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    descuento = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True, null=True)
+    IVA = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True, null=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True, null=True)
 
     def __str__(self):
         return f'Carrito: {self.id}'
--- factus/views.py
+++ factus/views.py
@@ -7,35 +7,29 @@ def index(request):
     products = Products.objects.all()
     return render(request, 'factus/index.html',{'products': products})
 
-
 def product_detail(request, product_id):
     product = Products.objects.get(id=product_id)
     return render(request, 'factus/product_detail.html', {'product': product})
 
-
-
-#carrito
-
 def add_to_cart(request, product_id):
     product = get_object_or_404(Products, id= product_id)
+    cantidad = request.POST.get('cantidad')
 
-    print(product)
+    #obtenemos el carrito
+    cart, created = Cart.objects.get_or_create(user=request.user)
 
-    cart, created = Cart.objects.get_or_create(user=request.user, total=0.00)
-
-    cart_product, created = CartProducts.objects.get_or_create(cart=cart, product=product)
-    
-    if not created:
-        cart_product.quantity += 1
+    #verificamos si el producto esta en el carrito
+    try:
+        cart_product = CartProducts.objects.get(cart = cart, product=product)
+        cart_product.quantity += cantidad
         cart_product.save()
+    except CartProducts.DoesNotExist:
+        cart_product = CartProducts.objects.create(cart = cart, product=product, quantity=cantidad)
 
-    else:
-        cart_product.quantity = 1
-        cart_product.save()
+    return redirect('cart')
 
 
-    cart.total = sum(item.product.price * item.quantity for item in cart.cartproducts.all())
-    cart.IVA = cart.total * 0.16
-    cart.save()
-
-    return redirect('detalle_carrito')
+def cart(request):
+    cart = Cart.objects.get(user=request.user)
+    cart_products = CartProducts.objects.filter(cart=cart)
+    return render(request, 'factus/carrito.html', {'cart': cart, 'cart_products': cart_products})
